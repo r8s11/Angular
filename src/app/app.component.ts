31a37a0750bcaa401280e32848d362df270f054c
rz843 <Roosevelt.bseg@gmail.com>
2020-08-26 16:32:44 -0400

    8/26

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+
 post = {
     title: "Title",
     isFavorite: true
   }
+onFavoriteChange(eventArgs){
+  console.log("Favorite changed: ", eventArgs)
+
+}
 }
--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
 
 @Component({
   selector: 'favorite',
@@ -6,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./favorite.component.css']
 })
 export class FavoriteComponent implements OnInit {
+  @Input() isFavorite: boolean;
+  @Output() change = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onClick(){
+    this.isFavorite = !this.isFavorite;
+    this.change.emit({newValue: this.isFavorite});
+  }
 }
